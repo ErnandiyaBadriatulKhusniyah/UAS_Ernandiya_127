8b13951ec1c2f8ddd2add928b33197fc766493c1
Ernandiya Badriatul Khusniyah <155121353+ErnandiyaBadriatulKhusniyah@users.noreply.github.com>
2024-07-26 23:54:17 +0700

    seventh changes

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(port, () => {
   console.log("API berjalan di : "+port);
 });
 
+app.use(cors());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
